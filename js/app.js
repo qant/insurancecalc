f1ee8e093c9ec2e3687111aaf7632f3daeb9807e
Anton Semenov <anseme@gmail.com>
2019-12-31 11:19:19 +0100

    Calculate total with proto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
-function Insurance(label, year, itype) {
+/**
+ *
+ * @param {string} label
+ * @param {string} year
+ * @param {int} type
+ */
+function Insurance(label, year, type) {
   this.label = label;
-  this.itype = itype;
   this.year = year;
+  this.type = type;
 }
+
+Insurance.prototype.calculate = function() {
+  const base = 2000;
+  let total;
+
+  switch (this.label) {
+    case "1": //americ
+      total = base * 1.15;
+      break;
+    case "2": //asiat
+      total = base * 1.05;
+      break;
+    case "3": //euro
+      total = base * 1.3;
+      break;
+  }
+
+  const diff = new Date().getFullYear() - this.year;
+  total = total - (diff * 3) / 100; //3% by year
+
+  if ("basico" === this.type) {
+    total = total * 1.3;
+  } else {
+    total = total * 1.5;
+  }
+
+  return total;
+};
 
 //To show
 function Interface() {}
@@ -42,6 +76,9 @@ function calcualteBtnAction(event) {
   } else {
     console.info("All fine created interface");
     interface.showError("Fields are OK, calculating...", "correcto");
+    const insurance = new Insurance(selectedL, selectedY, selectedT);
+    const total = insurance.calculate();
+    console.log(total);
   }
 }
 
